cfcebc3b782b57a3a6614399b10c9ccf0586b5d9
obb12 <omar.bb@hotmail.fi>
2018-11-16 14:58:37 +0200

    saved tweets from twitter api

--- src/config/express.js
+++ src/config/express.js
@@ -6,18 +6,13 @@ import twitterClient from './twitter'
 const app = express()
 
 app.get('/', async (req, res, next) => {
-  res.send({ message: 'hei' })
-  twitterClient.get(
-    'statuses/user_timeline',
-    { screen_name: 'Yliaho' },
-    (err, tweets) => {
-      if (err) {
-        throw err
-      }
+  TweetModel.find((err, people) => {
+    if (err) return res.status(500).send(err)
 
-      console.log(tweets)
-    }
-  )
+    // send the list of all people in database with name of "John James" and age of 36
+    // Very possible this will be an array with just one Person object in it.
+    return res.status(200).send(people)
+  })
 })
 
 export default app
--- src/jobs/getTweets/index.js
+++ src/jobs/getTweets/index.js
@@ -1,5 +1,7 @@
 import Job from '@/Job'
 import twitterClient from '@/config/twitter'
+import TweetModel from '@/models/tweet.model'
+
 import { readFileAsync } from '@/utils/'
 import path from 'path'
 
@@ -16,7 +18,28 @@ const getTweets = async () => {
         throw err
       }
 
-      console.log(tweets)
+      for (var i = 0; i < tweets.statuses.length; i++) {
+        var tweet = tweets.statuses[i]
+        TweetModel.find({ query_hashtag: hashtagQuery }, (err, hashtags) => {
+          if (hashtags.length == 0) {
+            tweets = new TweetModel({
+              query_hashtag: hashtagQuery,
+              text: tweet.text,
+              user: tweet.user,
+              id: tweet.id,
+              hashtags: [],
+              retweet_count: tweet.retweet_count,
+              favorite_count: tweet.favorite_count
+            })
+            if (tweets.retweet_count > 1) {
+              tweets.save(err => {
+                if (err) throw err
+                console.log('saved')
+              })
+            }
+          }
+        })
+      }
     })
   }
 }
