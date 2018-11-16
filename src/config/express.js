99600c7b2ace09b628cbdd423e4df1a96a038cd5
Joonas Yliaho <joonas.yliaho@gmail.com>
2018-11-16 12:04:01 +0200

    fetch user twitter timeline

--- src/config/express.js
+++ src/config/express.js
@@ -1,11 +1,23 @@
 // @ts-check
 import express from 'express'
 import TweetModel from '../models/tweet.model'
+import twitterClient from './twitter'
 
 const app = express()
 
 app.get('/', async (req, res, next) => {
   res.send({ message: 'hei' })
+  twitterClient.get(
+    'statuses/user_timeline',
+    { screen_name: 'Yliaho' },
+    (err, tweets) => {
+      if (err) {
+        throw err
+      }
+
+      console.log(tweets)
+    }
+  )
 })
 
 export default app
