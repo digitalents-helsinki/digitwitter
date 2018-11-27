378ba38cb897271ffaceccffffdcaf3b39ef8106
obb12 <omar.bb@hotmail.fi>
2018-11-27 13:36:04 +0200

    removed flat-map

--- src/config/slack.js
+++ src/config/slack.js
@@ -2,6 +2,8 @@
 import { slack } from '../constants/'
 import { WebClient, RTMClient } from '@slack/client'
 import TweetModel from '@/models/tweet.model'
+import { readFileAsync } from '@/utils/'
+import path from 'path'
 
 const token = slack.legacy_token
 
@@ -9,7 +11,7 @@ const web = new WebClient(token)
 const rtm = new RTMClient(token)
 rtm.start()
 
-const channel = 'G935ENCTS'
+const channel = 'CEA6SMCF7'
 
 export const sendMessage = () => {
   web.chat
@@ -21,26 +23,49 @@ export const sendMessage = () => {
       throw err
     })
 }
-TweetModel.findOne({ query_hashtag: 'slush18' }, function(err, person) {
-  if (err) return handleError(err)
-  web.chat
-    .postMessage({ channel, text: person.query_hashtag })
-    .then(res => {
-      //`res` contains information about the posted message
-      console.log('Message sent: ', res.ts)
-    })
-    .catch(console.error)
-  web.chat
-    .postMessage({ channel, text: person.text })
-    .then(res => {
-      //`res` contains information about the posted message
-      console.log('Message sent: ', res.ts)
-    })
-    .catch(console.error)
-})
+const postsTweets = async () => {
+  const hashtagQueryArray = await readFileAsync(
+    path.resolve(process.cwd(), 'data/query.json'),
+    { encoding: 'utf8' }
+  )
+  for (let hashtagQuery of JSON.parse(hashtagQueryArray)) {
+    //TweetModel.findOne({ query_hashtag: hashtagQuery }, function(err, person) {
+    //if (err) return handleError(err)
+    //web.chat
+    //.postMessage({ channel, text: 'https://twitter.com/search?q=' +person.query_hashtag })
+    //.then(res => {
+    //`res` contains information about the posted message
+    //    console.log('Message sent: ', res.ts)
+    //})
+    //.catch(console.error)
+    //})
+    TweetModel.find({
+      query_hashtag: hashtagQuery,
+      retweet_count: { $gt: 30, $lt: 66 }
+    }).exec(callback)
+  }
+}
+
+const callback = (err, data) => {
+  console.log(data)
+  for (let hashtags of data) {
+    console.log(hashtags.id)
+    web.chat
+      .postMessage({
+        channel,
+        text: 'https://twitter.com/search?q=' + hashtags.query_hashtag
+      })
+      .then(res => {
+        //`res` contains information about the posted message
+        console.log('Message sent: ', res.ts)
+      })
+      .catch(console.error)
+  }
+}
 
 export const listen = () => {
   rtm.on('message', e => {
     console.log(e)
   })
+  postsTweets()
 }
